5d025b5704550abae2c6e92218f5f975d2db6e00
Denis Markin <denismark94@gmail.com>
2024-08-26 23:33:31 +0300

    feat: add animation for slider, phone screen activation

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var section = document.querySelectorAll("main > div");
-var sections = {};
-var i = 0;
+let section = document.querySelectorAll("main > div");
+let sections = {};
+let i = 0;
 
 section.forEach(function(e) {
     sections[e.id] = e.offsetTop - 95;
   });
 
   window.onscroll = function() {
-    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
 
     for (i in sections) {
       if (sections[i] <= scrollPosition) {
@@ -18,3 +18,72 @@ section.forEach(function(e) {
       }
     }
   };
+
+const CAROUSEL = document.querySelector(".carousel");
+const LEFTARROW = document.querySelector(".left-chev");
+const RIGHTARROW = document.querySelector(".right-chev");
+const CAROUSEL_LEFT = document.querySelector(".carousel-left");
+const CAROUSEL_CENTER = document.querySelector(".carousel-center");
+const CAROUSEL_RIGHT = document.querySelector(".carousel-right");
+
+const set_listeners = () => {
+  document.querySelectorAll(".iphone").forEach((phone)=>{
+    phone.addEventListener('click', (event)=>{
+      event.currentTarget.querySelector('.iphone-bg').classList.toggle('off');
+    });
+  });
+};
+
+set_listeners();
+
+const moveLeft = () => {
+  CAROUSEL.classList.add("slide-left");
+  LEFTARROW.removeEventListener("click", moveLeft);
+  RIGHTARROW.removeEventListener("click", moveRight);
+};
+
+const moveRight = () => {
+  CAROUSEL.classList.add('slide-right');
+  LEFTARROW.removeEventListener("click", moveLeft);
+  RIGHTARROW.removeEventListener("click", moveRight);
+};
+
+LEFTARROW.addEventListener("click", moveLeft);
+RIGHTARROW.addEventListener("click", moveRight);
+
+
+CAROUSEL.onanimationend = (event) => {
+  switch(event.animationName) {
+    case "slide-left":
+      CAROUSEL.classList.remove('slide-left');
+      upd_left();
+      break;
+    case "slide-right":
+      CAROUSEL.classList.remove('slide-right');
+      upd_right();
+      break;
+  }
+  
+  LEFTARROW.addEventListener("click", moveLeft);
+  RIGHTARROW.addEventListener("click", moveRight);
+
+
+};
+
+const upd_left = () => {
+  let left_html = CAROUSEL_LEFT.innerHTML;
+  CAROUSEL_LEFT.innerHTML = CAROUSEL_CENTER.innerHTML;
+  CAROUSEL_CENTER.innerHTML = CAROUSEL_RIGHT.innerHTML;
+  CAROUSEL_RIGHT.innerHTML = left_html; 
+  set_listeners(); 
+}
+
+const upd_right = () => {
+  let center_html = CAROUSEL_CENTER.innerHTML;
+  CAROUSEL_CENTER.innerHTML = CAROUSEL_LEFT.innerHTML;
+  CAROUSEL_LEFT.innerHTML = CAROUSEL_RIGHT.innerHTML;
+  CAROUSEL_RIGHT.innerHTML = center_html;
+  set_listeners();
+}
+
+
